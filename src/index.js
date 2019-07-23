c85dc042cb7fb9eb5bd9cfa81a869f8eb4bc00d1
Cindy <cindyzhang977@gmail.com>
2019-07-22 21:03:40 -0700

    working tic tac toe game

--- src/index.js
+++ src/index.js
@@ -2,23 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    //calls onClick defined in renderSquare in Board
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props); //always need to call super when defining constructor of subclass
+    this.state = {                  //use state for the compenent to "remember" things
+      squares: Array(9).fill(null), //state considered private to the component
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice(); //create copy of squares to modify
+    //ignore click if square is full or if there's a Winner
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext? 'X' : 'O';
+    this.setState({squares: squares,
+                  xIsNext: !this.state.xIsNext}); //avoiding data mutation lets us keep track of game history
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status; //const makes contract that no rebinding will happen whereas there's no guarantee in let
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -65,3 +94,26 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  //all possible wins
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  //check each combination in lines to see if there's three in a row
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]; //this is winner
+    }
+  }
+  return null;
+}
